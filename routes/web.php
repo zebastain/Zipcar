ca466ecfa8b82b73ed4e4c261a78f6adf6d2834b
zebastain <artuzs@uninorte.edu.co>
2020-03-12 16:39:49 -0500

    Reservation model added

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/reservation', function () {
+    return view('reservation');
+});
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
