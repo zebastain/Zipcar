aabf45b8f5062c22e7b5e140bc9fdb1dbd08ec6a
dan3143 <vdguevara@uninorte.edu.co>
2020-03-11 20:58:25 -0500

    Aunthentication implemented

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('profile_picture')->default('user.png');
             $table->rememberToken();
             $table->timestamps();
+            $table->primary('username');
         });
     }
 
