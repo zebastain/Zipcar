fea14b14d27f737afa2b1a205f14277239fd43be
dan3143 <vdguevara@uninorte.edu.co>
2020-03-12 18:26:12 -0500

    Added migrations for carrs, car models and borrows

---
+++ database/migrations/2020_03_12_215236_create_cars_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("model");
+            $table->enum("availability", ["AVAILABLE", "UNAVALIABLE"]);
+            $table->enum("status", ["GOOD", "MEDIUM", "BAD"]);
+            $table->decimal("mileage");
+            $table->timestamps();
+            $table->foreign("model")->references("id")->on("models")
+                  ->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
